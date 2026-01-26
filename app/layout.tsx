0e601e0d8ca5482c4264a119df71ad24b1a2703d
Oven_Nopparut <nopparut2845@gmail.com>
2026-01-26 10:17:07 +0700

    Update page title to Noppharut Nuthueng

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Portfolio | My Portfolio",
+  title: "Noppharut Nuthueng",
   description: "Welcome to my portfolio website",
 };
 
